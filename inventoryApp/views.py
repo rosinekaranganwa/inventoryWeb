e1cfd8427bf011d7163a0c067f6662af4ad47e0e
karanganwa <karanganwatuyisenge@gmail.com>
2023-08-08 02:24:40 -0400

    implementing a view of adding a new book

--- inventoryApp/views.py
+++ inventoryApp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import CreateBookForm
 
-# Create your views here.
+def create_book(request):
+    if request.method=='POST':
+        form=CreateBookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+        
+    else:
+        form=CreateBookForm()
+    return render(request,'books/create_book.html',{'form':form})
+
